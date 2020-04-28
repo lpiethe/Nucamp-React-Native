ee18a89eb1bc84f3ea7baff8594223f1d746940b
Lindsay P <lpiethe@gmail.com>
2020-04-27 21:46:01 -0700

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -11,38 +12,59 @@ const mapStateToProps = state => {
 };
 
 class About extends Component {
-   
+
     static navigationOptions = {
         title: 'About Us'
     }
 
     render() {
-        const renderPartner = ({item}) => {
+        const renderPartner = ({ item }) => {
             return (
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: {uri: baseUrl + item.image}}}
+                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
                 />
             );
         };
+
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
         return (
             <ScrollView>
                 <Mission />
                 <Card title='Community Partners'>
                     <FlatList
-                        data ={this.props.partners.partners}
-                        keyExtractor ={item => item.id.toString()}
-                        renderItem ={renderPartner}
+                        data={this.props.partners.partners}
+                        keyExtractor={item => item.id.toString()}
+                        renderItem={renderPartner}
                     />
                 </Card>
             </ScrollView>
         );
-        
+
     }
 }
 
-function Mission () {
+function Mission() {
     return (
         <Card title=' Our Mission'>
             <Text>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
